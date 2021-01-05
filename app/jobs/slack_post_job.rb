030c3da5ee41df7a2f333e429f7b7eba3589207f
下田　佳紀 <y.s@platinum.local>
2021-01-05 13:02:47 +0900

    記述を修正/不要なincludeを削除

--- app/jobs/slack_post_job.rb
+++ app/jobs/slack_post_job.rb
@@ -1,11 +1,9 @@
 class SlackPostJob < ApplicationJob
-  include Clockwork
   queue_as :default
 
   def perform(bot_requirement)
     client = SlackPostJob.client
-    # client.notify(bot_requirement)
-    every(5.seconds, 'SlackPostJob') { Delayed::Job.enqueue(client.notify(Bot.find(rand(1..Bot.all.count)).requirement)) }
+    client.notify(bot_requirement)
   end
 
   def self.client
